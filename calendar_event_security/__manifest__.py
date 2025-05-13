e4fa1a15f9ea1df7c70d7589acf52a7824739bf7
Iria Alonso <iria.alonso@solvos.es>
2025-05-13 10:54:05 +0200

    [FIX] calendar_event_security: event followers or attendees also can modify it

--- calendar_event_security/__manifest__.py
+++ calendar_event_security/__manifest__.py
@@ -3,14 +3,14 @@
 {
     "name": "Calendar Event Security",
     "summary": """
-        By default, all users can modify/delete a calendar event if they are followers of the event or not.
-        To restrict that, now only the followers of an calendar event will be able to modify/delete it.
-        Also, there is a new group in "Extra Rights" that allows a user, follower or not, to modify/delete an calendar event.
+        By default, all users can modify/delete a calendar event if they are followers or attendees of the event or not.
+        To restrict that, now only the followers or attendees of an calendar event will be able to modify/delete it.
+        Also, there is a new group in "Extra Rights" that allows a user, follower/attendee or not, to modify/delete an calendar event.
         By default, user admin will be able to modify and delete calendar events.
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Account",
     "website": "https://github.com/solvosci/slv-calendar",
     "depends": [
